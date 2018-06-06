bf38694a9be6b212e58ac7e73dd09e9f122bd1ed
Todd Romkema <todd.romkema@gmail.com>
2018-06-06 10:01:03 -0500

    Initial commit

---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig
+        implements WebMvcConfigurer
+{
+
+    // The Controller situation could instead by handled by using the @Controller tag on a controller class that...
+            // can return Strings (that have the RequestMapping tag) like public String hello(Model model);
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
